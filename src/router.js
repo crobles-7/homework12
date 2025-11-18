ec2754a3d01fffcff3d711a466c37b61665db780
Carlos Robles <carlos.robles97@gmail.com>
2025-11-18 16:38:08 -0600

    Navigation is working

--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
+import MealsByName from './views/MealsByName.vue'
+import MealsByLetter from './views/MealsByLetter.vue'
+import MealsByIngredients from './views/MealsByIngredients.vue'
+import MealDetails from './views/MealDetails.vue'
+import Ingredients from './views/Ingredients.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/homework12/', name: 'Home', component: HomePage },
+  { path: '/homework12/by-name/:name?', name: 'MealsByName', component: MealsByName },
+  { path: '/homework12/by-letter/:letter?', name: 'MealsByLetter', component: MealsByLetter },
+  { path: '/homework12/by-ingredient/:ingredient?', name: 'MealsByIngredients', component: MealsByIngredients },
+  { path: '/homework12/meal/:id', name: 'MealDetails', component: MealDetails },
+  { path: '/homework12/ingredients', name: 'Ingredients', component: Ingredients },
+  { path: '/homework12/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
 ]
 
 const router = createRouter({
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/homework12/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -11,3 +12,4 @@ export default defineConfig({
     },
   },
 })
+
