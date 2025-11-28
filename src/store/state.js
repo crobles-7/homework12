8f1b856c7a85ff1cd228faf8367f5f9b8e8a86bc
Carlos Robles <carlos.robles97@gmail.com>
2025-11-28 15:14:14 -0600

    Data store and meals by letter complete

---
+++ src/store/state.js
@@ -0,0 +1,9 @@
+import MealsByIngredients from "@/views/MealsByIngredients.vue";
+import MealsByLetter from "@/views/MealsByLetter.vue";
+
+export default {
+    searchedMeals: [],
+    MealsByLetter: [],
+    MealsByIngredient: [],
+    ingredient: {},
+}
--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -33,5 +33,6 @@ onMounted(() => {
 
     </div>
 
+
     
 </template>
