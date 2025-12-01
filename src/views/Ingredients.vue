d02116cb5bdf08931f7aecd743d6e589e35961d8
Carlos Robles <carlos.robles97@gmail.com>
2025-12-01 12:43:57 -0600

    Getting ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -28,19 +28,4 @@ const searchIngredients = computed(() => {
     </div>
 
     <!-- {{ ingredients }} -->
-
-    <div class="p-10">
-        <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full bg-white mb-5" placeholder="Enter ingredient name" />
-
-        <div class="grid grid-cols-4 gap-5">
-            <router-link 
-                v-for="ingredient in searchIngredients" 
-                :key="ingredient.idIngredient" 
-                :to="{name: 'MealsByIngredients', params: {ingredient: ingredient.strIngredient}}" 
-                class="bg-white rounded p-3 mb-3 shadow block">
-                    <img :src="`http://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`" />
-                    <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
-            </router-link>
-        </div>
-    </div>
 </template>
